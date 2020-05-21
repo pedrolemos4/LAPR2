56e41f1b715553d76d5e0d2d4d3938b8e48bccde
cgf <cgf@isep.ipp.pt>
2020-05-21 20:11:21 +0100

    Example

--- src/main/java/pt/ipp/isep/dei/esoft/pot/ui/Main.java
+++ src/main/java/pt/ipp/isep/dei/esoft/pot/ui/Main.java
@@ -17,7 +17,7 @@ public class Main
     public static void main(String[] args)
     {
         try
-        {   // Exemplo
+        {   
             MenuUI uiMenu = new MenuUI();
 
             uiMenu.run();
