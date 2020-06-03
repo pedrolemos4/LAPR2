23bf20d806b44860855c98ad00772dbdfa484539
1190974 <1190974@isep.ipp.pt>
2020-06-03 12:31:58 +0100

    Add Java interface Collaborator Options

---
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/controller/JanelaOptionsCollaborator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lapr2_interfacegrafica.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+/**
+ *
+ * @author pedro
+ */
+public class JanelaOptionsCollaborator {
+
+    @FXML
+    private void selectFreelancer(MouseEvent event) {
+    }
+
+    @FXML
+    private void createTask(MouseEvent event) {
+    }
+
+    @FXML
+    private void createPayment(MouseEvent event) {
+    }
+
+    @FXML
+    private void uploadFile(MouseEvent event) {
+    }
+
+    @FXML
+    private void back(MouseEvent event) {
+    }
+    
+}
