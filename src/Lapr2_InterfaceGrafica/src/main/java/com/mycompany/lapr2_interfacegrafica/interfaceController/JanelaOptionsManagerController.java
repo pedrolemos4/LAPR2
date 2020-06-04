2e19375a37e750fcb60cc0bb166d89b1e4d00cd2
Tiago_1191098 <1191098@isep.ipp.pt>
2020-06-04 09:59:33 +0100

    Scenes for Graphic Interface [Implementation]

---
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/interfaceController/JanelaOptionsManagerController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lapr2_interfacegrafica.interfaceController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author Tiago
+ */
+public class JanelaOptionsManagerController {
+
+    @FXML
+    private Button btnSetDayOfPayment;
+    @FXML
+    private Button btnCheckStatistics;
+
+    @FXML
+    private void btnSetDayOfPaymentAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCheckStatisticsAction(ActionEvent event) {
+    }
+    
+}
---
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/interfaceController/SetDayOfPaymentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lapr2_interfacegrafica.interfaceController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Tiago
+ */
+public class SetDayOfPaymentController {
+
+    @FXML
+    private Label lblSetDayofPayment;
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Label lblTimeOfTheDay;
+    @FXML
+    private TextField txtDate;
+    @FXML
+    private TextField txtTimeOfTheDay;
+    @FXML
+    private Button btnConfirm;
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private void btnConfirmAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCancelAction(ActionEvent event) {
+    }
+    
+}
