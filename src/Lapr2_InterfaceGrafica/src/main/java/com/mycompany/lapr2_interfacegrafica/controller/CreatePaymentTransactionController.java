ced49b7a577af097cd27544bd955330b0154ddb4
1190974 <1190974@isep.ipp.pt>
2020-06-04 09:35:52 +0100

    Change Java imports

--- src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/controller/CreatePaymentTransactionController.java
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/controller/CreatePaymentTransactionController.java
@@ -2,22 +2,22 @@ package com.mycompany.lapr2_interfacegrafica.controller;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import lapr2.autorizacao.model.SessaoUtilizador;
-import lapr2.pot.model.Freelancer;
+import com.mycompany.lapr2_interfacegrafica.authorization.model.UserSession;
+import com.mycompany.lapr2_interfacegrafica.model.Freelancer;
 import lapr2.pot.model.FreelancersRecord;
 import com.mycompany.lapr2_interfacegrafica.model.Organization;
 import com.mycompany.lapr2_interfacegrafica.model.OrganizationsRecord;
 import com.mycompany.lapr2_interfacegrafica.model.PaymentTransaction;
 import com.mycompany.lapr2_interfacegrafica.model.PaymentTransactionList;
-import lapr2.pot.model.Plataforma;
-import lapr2.pot.model.Task;
+import com.mycompany.lapr2_interfacegrafica.model.Plataforma;
+import com.mycompany.lapr2_interfacegrafica.model.Task;
 import lapr2.pot.ui.console.utils.Utils;
 
 public class CreatePaymentTransactionController {
 
     private AplicacaoPOT m_oApp;
     private Plataforma m_oPlataforma;
-    private SessaoUtilizador m_oSessao;
+    private UserSession m_oSessao;
     private OrganizationsRecord or;
     //private TasksList tLst;
     private FreelancersRecord frlR;
