08a2ab5bb4fef52d5395d7e8e941f9d804d50935
Tiago_1191098 <1191098@isep.ipp.pt>
2020-06-05 21:38:44 +0100

    AddNewOrganization Scene for graphic interface [Implementation]

---
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/ui/AddNewOrganizationUI.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lapr2_interfacegrafica.ui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Tiago
+ */
+public class AddNewOrganizationUI {
+
+    @FXML
+    private Button btnNext;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Label lblAlert;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtNIF;
+
+    @FXML
+    private void btnNextAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCancelAction(ActionEvent event) {
+    }
+    
+}
