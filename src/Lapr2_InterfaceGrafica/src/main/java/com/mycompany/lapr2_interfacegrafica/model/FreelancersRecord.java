fe165da4ad2c33c89e5c529789c39dfc0bebd04e
1190974 <1190974@isep.ipp.pt>
2020-06-05 11:36:40 +0100

    Change contructor

--- src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/model/FreelancersRecord.java
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/model/FreelancersRecord.java
@@ -1,5 +1,6 @@
 package com.mycompany.lapr2_interfacegrafica.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FreelancersRecord {
@@ -15,8 +16,8 @@ public class FreelancersRecord {
 
     private List<Freelancer> arrayFreelancers;
 
-    public FreelancersRecord(String id, String name, String lvlExp, String email, String nif, String iban, String country, String adress) {
-        Freelancer freelancer = new Freelancer(id, name, lvlExp, email, nif, iban, country, adress);
+    public FreelancersRecord() {
+        this.arrayFreelancers = new ArrayList<>();
     }
 
     public Freelancer newFreelancer(String id, String name, String lvlExp, String email, String nif, String iban, String country, String adress) {
