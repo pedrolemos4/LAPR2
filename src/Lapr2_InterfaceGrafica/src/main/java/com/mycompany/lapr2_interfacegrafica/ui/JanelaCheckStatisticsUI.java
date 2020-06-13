e483782a137156ce1bfb9ba51e65f8e2f7ffdcc1
Tiago_1191098 <1191098@isep.ipp.pt>
2020-06-13 19:36:55 +0100

    Changes related to statistics and implementation of automatically tasks' payments [Implementation]

--- src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/ui/JanelaCheckStatisticsUI.java
+++ src/Lapr2_InterfaceGrafica/src/main/java/com/mycompany/lapr2_interfacegrafica/ui/JanelaCheckStatisticsUI.java
@@ -7,13 +7,16 @@ package com.mycompany.lapr2_interfacegrafica.ui;
 
 import com.mycompany.lapr2_interfacegrafica.controller.CheckStatisticsController;
 import java.io.IOException;
+import java.util.TreeMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -22,22 +25,92 @@ import javafx.stage.Stage;
  */
 public class JanelaCheckStatisticsUI {
 
+    private JanelaOptionsCollaboratorScene_UI optionCollabUI;
+    private JanelaOptionsManagerScene_UI optionsManagerUI;
     private CheckStatisticsController controller;
+    private Stage statisticsStage;
 
     @FXML
-    private ListView<?> lstFreelancers;
+    private ListView<TreeMap<String, Double>> lstFreelancersAndInfos;
+    @FXML
+    private Button btnBack;
+    @FXML
+    private Button btnMeanDelay;
+    @FXML
+    private Button btnDeviationDelay;
+    @FXML
+    private Button btnMeanPayment;
+    @FXML
+    private Button btnDeviationPayment;
+    @FXML
+    private Button btnClean;
 
-    public void goToScene(ActionEvent event, String fxml) throws IOException {
-        Parent button = FXMLLoader.load(getClass().getResource(fxml));
-        Scene buttonScene = new Scene(button);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(buttonScene);
-        window.show();
-        setController();
+    public void goToSceneAlt(JanelaOptionsCollaboratorScene_UI menuCollaboratorUI) throws IOException {
+        this.optionCollabUI = menuCollaboratorUI;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/JanelaCheckStatistics.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        statisticsStage = new Stage();
+        statisticsStage.initModality(Modality.APPLICATION_MODAL);
+        statisticsStage.setScene(scene);
+        System.out.println("Será que mostrou?");
+        loader.setController(this);
+        statisticsStage.show();
+        //setComboBoxFreelancer(transactionStage, scene);
+//        transactionStage.setScene(scene);
+//        transactionStage.show();
+    }
+
+    public void goToSceneAlt(JanelaOptionsManagerScene_UI menuManagerUI) throws IOException {
+        this.optionsManagerUI = menuManagerUI;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/JanelaCheckStatistics.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        statisticsStage = new Stage();
+        statisticsStage.initModality(Modality.APPLICATION_MODAL);
+        statisticsStage.setScene(scene);
+        System.out.println("Será que mostrou?");
+        loader.setController(this);
+        statisticsStage.show();
+        //setComboBoxFreelancer(transactionStage, scene);
+//        transactionStage.setScene(scene);
+//        transactionStage.show();
     }
 
     public void setController() {
         this.controller = new CheckStatisticsController();
     }
 
+    @FXML
+    private void btnBackAction(ActionEvent event) throws IOException {
+        goToScene(event, "/fxml/OptionsCollaborator.fxml");
+    }
+
+    public void goToScene(ActionEvent event, String fxml) throws IOException {
+        Parent button = FXMLLoader.load(getClass().getResource(fxml));
+        //  System.out.println("Será que mostrou?");
+        Scene buttonScene = new Scene(button);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(buttonScene);
+        //System.out.println("Vais Mostrar?");
+        window.show();
+    }
+
+    @FXML
+    private void meanDelayAction(ActionEvent event) {
+        TreeMap<String, Double> lstMeanDelay;
+
+    }
+
+    @FXML
+    private void btnDeviationDelayAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnDeviationPaymentAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnCleanAction(ActionEvent event) {
+    }
 }
